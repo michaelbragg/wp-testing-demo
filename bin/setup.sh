1d98e2e25955cc8f7386a9e153ad5477665346c7
michaelbragg <michael@michaelbragg.com>
2021-11-05 10:46:31 +0000

    [Added] WooCommerce setup

---
+++ bin/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Activate theme.
+npm run wp-env run cli "wp theme activate storefront --quiet"
+
+# Activate plugins.
+npm run wp-env run cli "wp plugin activate woocommerce --quiet"
+npm run wp-env run cli "wp plugin activate woocommerce-gateway-dummy --quiet"
+
+# Empty Blog Description
+npm run wp-env run cli 'wp option update blogdescription ""'
+# Set permalinks
+npm run wp-env run cli 'wp option update permalink_structure "/%postname%/"'
+
+# Import dummy data.
+npm run wp-env run cli "wp plugin install wordpress-importer --activate"
+npm run wp-env run cli "wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=create"
+npm run wp-env run cli "wp plugin deactivate wordpress-importer --uninstall"
