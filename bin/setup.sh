87de525850394be4ee1596e76a9c96688ce1cc69
Michael Bragg <michael@michaelbragg.com>
2024-06-27 07:49:51 +0000

    Fixed: Setup WP-CLI commands not running

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Activate theme.
-npm run wp-env run cli "wp theme activate storefront --quiet"
+npm run wp-env run cli wp theme activate storefront
 
 # Activate plugins.
-npm run wp-env run cli "wp plugin activate woocommerce --quiet"
-npm run wp-env run cli "wp plugin activate woocommerce-gateway-dummy --quiet"
+npm run wp-env run cli wp plugin activate woocommerce
+npm run wp-env run cli wp plugin activate woocommerce-gateway-dummy
 
 # Empty Blog Description
-npm run wp-env run cli 'wp option update blogdescription ""'
+npm run wp-env run cli wp option update blogdescription ""
 # Set permalinks
-npm run wp-env run cli 'wp option update permalink_structure "/%postname%/"'
+npm run wp-env run cli wp option update permalink_structure "/%postname%/"
 
 # Import dummy data.
-npm run wp-env run cli "wp plugin install wordpress-importer --activate"
-npm run wp-env run cli "wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=create"
-npm run wp-env run cli "wp plugin deactivate wordpress-importer --uninstall"
+npm run wp-env run cli wp plugin install wordpress-importer --activate
+npm run wp-env run cli wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=create
+npm run wp-env run cli wp plugin deactivate wordpress-importer --uninstall
